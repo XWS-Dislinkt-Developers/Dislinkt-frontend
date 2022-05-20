c443ba3c3bfc916670bb05895f79fd85f2d90cd2
mihailomajstorovic47 <73445435+mihailomajstorovic47@users.noreply.github.com>
2022-05-21 00:00:00 +0200

    fix: email and password validation
    added validation for passwords containing username and not allowing 3 repeating characters in a row

--- Frontend/src/app/shared-components/modals/registration-modal/registration-modal.component.ts
+++ Frontend/src/app/shared-components/modals/registration-modal/registration-modal.component.ts
@@ -3,8 +3,6 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 import { map } from 'rxjs';
 import { AuthenticationServiceService } from 'src/app/services/authentication-service.service';
 
-
-
 @Component({
   selector: 'app-registration-modal',
   templateUrl: './registration-modal.component.html',
@@ -32,8 +30,10 @@ export class RegistrationModalComponent implements OnInit {
       username:['',[ Validators.required, Validators.pattern('[a-zA-Z0-9]*')]],
     //  username:['',[ Validators.required, Validators.pattern('[a-zA-Z0-9]*'), this.validateUsername.bind(this)]],
      name:['',[ Validators.required, Validators.pattern('[a-zA-Z ]*')]], 
-       email:['', [Validators.required, Validators.email, Validators.pattern('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')]],
-      password: ['', [Validators.required, Validators.minLength(10)]],// Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')]],
+       email:['', [Validators.required, Validators.email, Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}')]],
+       password: [  '', [
+        Validators.required, Validators.minLength(10),
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+~-])[A-Za-z\d!@#$%^&*()_+~-].{9,}') ]  ],
       confirmPassword: ['', [Validators.required, Validators.minLength(10)]],
     },
     { 
@@ -44,7 +44,34 @@ export class RegistrationModalComponent implements OnInit {
   
   }
 
+  doesPasswordRepeatCharacters() {
+    let passString = this.password.toString()
+    for (let i = 1; i < passString.length - 1; i++) {
+      if (i == passString.length - 2) {
+        if (passString.substring(i-1,i) == passString.substring(i,i+1) && passString.substring(i,i+1) == passString.substring(i+1))
+          return true
+      }
+      else if (passString.substring(i-1,i) == passString.substring(i,i+1) && passString.substring(i,i+1) == passString.substring(i+1,i+2))
+        return true
+    }
+    return false
+  }
+
+  doesPasswordContainUsername () {
+    if (this.password.toString().includes(this.username) && this.username != "")
+      return true
+    return false
+  }
+
 register(){
+  if (this.doesPasswordContainUsername()){ 
+    alert ("Password contains your username. Please, pick another password.")
+    return
+    }
+  if (this.doesPasswordRepeatCharacters()) {
+    alert ("You have too many repeating characters in a row. Please, pick another password.")
+    return
+  }
   console.log("Usao sam u metodu za registraciju")
   this.submitted = true
   if(this.registerForm.invalid ){
