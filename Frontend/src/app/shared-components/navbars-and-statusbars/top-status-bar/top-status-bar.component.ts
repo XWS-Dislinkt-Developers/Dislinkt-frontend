708aaad63d246e432db811e8fa8bc832f6b2cbd6
nevenaatic <atic.ra67.2018@uns.ac.rs>
2022-05-22 02:00:53 +0200

    feat: changes

--- Frontend/src/app/shared-components/navbars-and-statusbars/top-status-bar/top-status-bar.component.ts
+++ Frontend/src/app/shared-components/navbars-and-statusbars/top-status-bar/top-status-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationServiceService } from 'src/app/services/authentication-service.service';
 
 @Component({
   selector: 'app-top-status-bar',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-status-bar.component.css']
 })
 export class TopStatusBarComponent implements OnInit {
-
-  constructor() { }
+  admin: boolean=false;
+  user: boolean=false;
+  constructor(private router: Router, private authService: AuthenticationServiceService) { }
 
   ngOnInit(): void {
+    this.admin = this.authService.adminAccess()
+    console.log(this.admin)
+    this.user = this.authService.userAccess()
+    console.log(this.user)
   }
 
 }
